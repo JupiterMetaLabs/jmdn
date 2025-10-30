c5a9430c1e9f21e6acfb966a52eb40aed482c7df
Neeraj Chowdary <57310710+neerajchowdary889@users.noreply.github.com>
2025-10-30 20:16:21 +0530

    BLS changes - debugging

--- AVC/BuddyNodes/MessagePassing/BLS_Signer/Signer.go
+++ AVC/BuddyNodes/MessagePassing/BLS_Signer/Signer.go
@@ -1,10 +1,12 @@
 package BLS_Signer
 
 import (
+	"encoding/hex"
 	"fmt"
 	blssign "gossipnode/AVC/BLS/bls-sign"
-	"gossipnode/config/utils"
 	"strconv"
+	"sync"
+	"gossipnode/config/utils"
 )
 
 type BLSresponse struct {
@@ -14,38 +16,54 @@ type BLSresponse struct {
 	PeerID    string
 }
 
-// Service functions for the BLSresponse struct
-func SignMessage(vote int8) (BLSresponse, bool, error) {
+// cached BLS keypair (per-process)
+var (
+	blsOnce sync.Once
+	blsPriv []byte
+	blsPub  []byte
+	blsErr  error
+)
 
+func getBLSKeypair(rawPriv []byte) ([]byte, []byte, error) {
+	blsOnce.Do(func() {
+		blsPriv, blsPub, blsErr = blssign.GenerateBLSKeyPairFromRawPrivKey(rawPriv)
+	})
+	return blsPriv, blsPub, blsErr
+}
+
+// Service functions for the BLSresponse struct
+// Signs canonical message "vote:<v>" for BOTH 1 and -1
+func SignMessage(vote int8) (BLSresponse, bool, error) {
 	if vote != -1 && vote != 1 {
 		return *NewBLSresponseBuilder(nil), false, fmt.Errorf("invalid vote")
 	}
-
-	voteString := strconv.Itoa(int(vote))
 
 	privKey, err := utils.ReturnPrivateKey()
 	if err != nil {
 		return *NewBLSresponseBuilder(nil), false, err
 	}
 
-	privKeyBytes, err := privKey.Raw()
+	rawPriv, err := privKey.Raw()
 	if err != nil {
 		return *NewBLSresponseBuilder(nil), false, err
 	}
 
-	pubkey, err := utils.ReturnPublicKeyString()
+	priv, pub, err := getBLSKeypair(rawPriv)
 	if err != nil {
 		return *NewBLSresponseBuilder(nil), false, err
 	}
 
-	sig, err := blssign.BLSSign(privKeyBytes, []byte(voteString))
+	msg := []byte("vote:" + strconv.Itoa(int(vote)))
+	sig, err := blssign.BLSSign(priv, msg)
 	if err != nil {
 		return *NewBLSresponseBuilder(nil), false, err
 	}
+
+	pubHex := hex.EncodeToString(pub)
 
 	return *NewBLSresponseBuilder(nil).
-		SetSignature(string(sig)).
-		SetAgree(true).
-		SetPubKey(pubkey).
+		SetSignature(hex.EncodeToString(sig)).
+		SetAgree(vote == 1).
+		SetPubKey(pubHex).
 		Build(), true, nil
 }
