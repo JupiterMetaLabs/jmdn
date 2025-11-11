dab928a5e395d684c4bd14ce1b7e32b61630e12c
Neeraj Chowdary <57310710+neerajchowdary889@users.noreply.github.com>
2025-11-11 11:50:22 +0530

    Enable monitoring services in docker-compose.yml and update Prometheus configuration for improved metrics collection
    - Uncommented and configured Prometheus, Grafana, and Node Exporter services in docker-compose.yml for monitoring.
    - Updated Prometheus scrape intervals and timeouts in prometheus.yml for more frequent data collection.
    - Changed target addresses in prometheus.yml to 'host.docker.internal' for better accessibility in containerized environments.
    - Added Loki and Promtail services for enhanced logging capabilities.

--- fastsync/fastsync.go
+++ fastsync/fastsync.go
@@ -1013,14 +1013,14 @@ func (fs *FastSync) batchCreateOrderedWithRetry(entries []struct {
 			var newClient *config.PooledConnection
 			var clientErr error
 			if dbType == MainDB {
-				newClient, clientErr = DB_OPs.GetMainDBConnection()
+				newClient, clientErr = DB_OPs.GetMainDBConnection(context.Background())
 				if clientErr == nil {
 					DB_OPs.Close(fs.mainDB.Client)
 					DB_OPs.PutMainDBConnection(fs.mainDB)
 					fs.mainDB = newClient
 				}
 			} else {
-				newClient, clientErr = DB_OPs.GetAccountsConnection()
+				newClient, clientErr = DB_OPs.GetAccountsConnections(context.Background())
 				if clientErr == nil {
 					DB_OPs.Close(fs.accountsDB.Client)
 					DB_OPs.PutAccountsConnection(fs.accountsDB)
